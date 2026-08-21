21e08ae7f5f708a13ad17cc09873d53ee42410c2
Kunaai Koitikei <85220088+kunaaisylvia@users.noreply.github.com>
2026-08-21 05:43:17 +0300

    feat(wallet): add Spring Boot application entrypoint

---
+++ wallet-service/src/main/java/com/tinah/wallet/WalletServiceApplication.java
@@ -0,0 +1,3 @@
+package com.tinah.wallet;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class WalletServiceApplication {public static void main(String[] args){SpringApplication.run(WalletServiceApplication.class,args);}}
