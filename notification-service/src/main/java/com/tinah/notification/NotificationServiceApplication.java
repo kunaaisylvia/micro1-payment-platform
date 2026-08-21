17925ade361da032c376f302699955f65cda870b
Kunaai Koitikei <85220088+kunaaisylvia@users.noreply.github.com>
2026-08-21 05:43:41 +0300

    feat(notification): add Spring Boot application entrypoint

---
+++ notification-service/src/main/java/com/tinah/notification/NotificationServiceApplication.java
@@ -0,0 +1,3 @@
+package com.tinah.notification;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class NotificationServiceApplication {public static void main(String[] args){SpringApplication.run(NotificationServiceApplication.class,args);}}
